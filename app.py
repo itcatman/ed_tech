80d4f051689e1c1905b0c214f8e49d9139481ea2
ibran <imran@in-dust.tk>
2021-04-09 20:58:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask
+
 app = Flask(__name__)
 
+#Переменные
+server_time = datetime.datetime.now()
+Response_time = 'Coming soon'
+
+#Routes
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/status')
+def status():
+    return 'Server time:' + str(server_time) + ' '
+    return 'Response time:' + str(Response_time) + ''
+@app.route('/api/')
 
 if __name__ == '__main__':
     app.run()
