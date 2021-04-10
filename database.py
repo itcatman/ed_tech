bbb91d2a8bb14023448ce2d0a5110b331c8922a8
Abanazarъ <exgumacia@mail.ru>
2021-04-10 03:51:55 +0300

    Добавил немножко магии автоматизации

--- database.py
+++ database.py
@@ -1,4 +1,16 @@
+"""json-server db.json команда для запуска сервера и доступа к БД"""
+import requests, subprocess
 
+
+url = "http://localhost:3000/users" 
+subprocess.call(['json-server', 'db.json'])
+
+
+def get_req():
+    return requests.get(url).json
+
+def post_req(data):
+    requests.post(url, json=data)
 
 def check_mail(mail):
     #TODO:Если всё ок, return 'OK'
@@ -7,3 +19,4 @@ def check_mail(mail):
 def check_nickname(nick):
     #TODO: Если всё ок, return 'OK'
     return 'OK'
+
