befadf508c5c514113fed54da5ebf89bf8ee966e
Abanazarъ <exgumacia@mail.ru>
2021-04-10 18:17:25 +0300

    Spasite

--- models.py
+++ models.py
@@ -1,17 +1,13 @@
 class User():
-    def __init__(self, name, password, birthday, school_class, role):
+    def __init__(self, name, password):
         self.name = name
         self.password = password
-        self.birthday = birthday
-        self.school_class = school_class
-        self.role = role
 
-    def __str__(self):
-        return 'Name: {0}\nPassword: {1}\nBirthday: {2}\nClass: {3}\nRole: {4}'.format(
-            self.name,
-            self.password,
-            self.birthday,
-            self.school_class,
-            self.role
-        )
+    def get_json(self):
+        data = {
+            'name': self.name,
+            'password': self.password,
+        }
+        return data
+
 
