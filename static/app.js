48d5d701723e90c8c4cde2b6de796adc7da65dc8
AliShah05 <alishahbazov2003@yandex.ru>
2021-04-11 10:41:55 +0300

    lup

--- static/app.js
+++ static/app.js
@@ -1,29 +1,29 @@
 $(function() {
     $('#swal-1').click(function() {
-        $('.modal').toggleClass('show');
+        $('.modal-1').toggleClass('show');
     });
 });
 $(function() {
     $('#swal-2').click(function() {
-        $('.modal').toggleClass('show');
+        $('.modal-2').toggleClass('show');
     });
 });
 $(function() {
     $('#swal-3').click(function() {
-        $('.modal').toggleClass('show');
+        $('.modal-3').toggleClass('show');
     });
 });
 
 $(function() {
     $('#swal-4').click(function() {
-        $('.modal').toggleClass('show');
+        $('.modal-4').toggleClass('show');
     });
 });
 
 
 $(function() {
     $('.close').click(function() {
-        $('.modal').toggleClass('show');
+        $('.modal').removeClass('show');
     });
 });
 
