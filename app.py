82d00f73dc1ffaae4a875b4e570436d2b48087e3
Abanazarъ <exgumacia@mail.ru>
2021-04-11 12:10:54 +0300

    Press F

--- app.py
+++ app.py
@@ -1,29 +1,52 @@
-from flask import Flask, render_template, request, url_for, session , redirect, make_response
-import requests
- 
+from flask import Flask, render_template, request 
+import random, os
+
 
 app = Flask(__name__)
-app.secret_key = 'jd0sa1wjsoidn-ada-sdasdbdadbpd1'
+
+users = [
+    'Алия Анесина', 'Абдуллаев Рамазан', 'Викторий Багров', 'Али Шахбазов', 
+    'Анюта Николаевна', 'Расул Камзаков', 'Мария Александровна', 'Миша Бабухин', 
+    'Никита Баженов', 'Раиса Валерьевна', 'Витя Байков', 'Гаджи Магомедов'
+]
+
+head = random.choice(users)
+name = head
+level = 'Ученик lvl ' + str(random.randint(1, 10))
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    if 'username' in session:
-        return render_template('index.html', name=session['username'])
-    else:
-        return render_template('index.html')
+        return render_template('index.html', head=head, name=name, level=level)
 
 
 @app.route('/register/', methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        session['username'] = request.form['username']
-        session['password'] = request.form['password']
-        return render_template('index.html', name=session['username'])
-    else:
-        return render_template('register.html')
+    return render_template('register.html')
+
+
+@app.route('/main-2.html')
+def interes():
+    return render_template('main-2.html', name=name, level=level)
+
+
+@app.route('/main-3.html')
+def favorites():
+    return render_template('main-3.html', name=name, level=level)
+
+
+@app.route('/main-4.html')
+def my_class():
+    return render_template('main-4.html', name=name, level=level)
+
+
+@app.route('/main-5.html')
+def achivment():
+    return render_template('main-5.html', name=name, level=level)
+
 
 
 if __name__ == '__main__':
+    app.secret_key = os.urandom(12)
     app.run(debug=True)
